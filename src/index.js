62460c0367f3ba3f5b9df6d1afbe6c47dae6b825
Endzelisp <endzelisp@gmail.com>
2023-10-27 22:30:47 -0400

    Simplify business logic using  hidden property on each tabpanel

--- src/index.js
+++ src/index.js
@@ -1,88 +1,43 @@
 import './styles.css'
-import { homeElements } from './home';
-import { menuElements } from './menu';
-import { contactElements } from './contact';
+import homeSection from './home.js';
+import menuSection from './menu';
+import contactSection from './contact';
 
-// navigation list elements
-const navElemHome = document.querySelector('#main-nav-bar #home');
-const navElemMenu = document.querySelector('#main-nav-bar #menu');
-const navElemContact = document.querySelector('#main-nav-bar #contact');
+// navigation tab buttons
+const homeTabBtn = document.querySelector('#main-nav-bar #home');
+const menuTabBtn = document.querySelector('#main-nav-bar #menu');
+const ContactTabBtn = document.querySelector('#main-nav-bar #contact');
 
 // main area where the content is displayed
 const divContentArea = document.querySelector('div#content-area')
 
-/**
- * 
- * @param {string} elemID 'HTML Element ID'
- * @param {Array.<HTMLElement>} elementContent 'Array of HTML elements'
- * @param {boolean} isVisible 'True if the element must be visible, false otherwise'
- * 
- */
-const createElementContainer = function(elemID, elementContent) {
 
-  let isElemVisible = false;
-  const id = elemID;
-  // Set HTML element with classes and content
-  const div = document.createElement('div');
-  div.id = elemID;
-  div.classList.add('fill-viewport', 'non-visible');
-  elementContent.forEach((elem) => div.appendChild(elem));
-
-  return {
-    getHtmlElement: function() {
-      return div;
-    },
-    setVisibility: function(isVisible) {
-      if (isVisible) {
-        isElemVisible = true;
-        div.classList.remove('non-visible');
-      }
-      else {
-        isElemVisible = false;
-        div.classList.add('non-visible');
-      }
-    },
-    isVisible: function() {
-      return isElemVisible;
-    },
-    getID: function() {
-      return id;
-    }
-
-  };
-}
-
-const pagesArray = [
-  createElementContainer('home-content', homeElements),
-  createElementContainer('menu-content', menuElements),
-  createElementContainer('contact-content', contactElements)
+const tabArray = [
+  homeSection,
+  menuSection,
+  contactSection,
 ]
 
-pagesArray.forEach((elem, index) => {
-  if (index === 0) {
-    elem.setVisibility(true)
-  }
-  divContentArea.appendChild(elem.getHtmlElement())
-})
+divContentArea.append(...tabArray)
 
 /**
  *  @param {string} elemID 'Element ID'
  */
-const displayElement = function(elemID) {
-  pagesArray.forEach(elem => {
-    if (elem.getID() === elemID) elem.setVisibility(true);
-    else elem.setVisibility(false);
+const displayTab = function(elemID) {
+  tabArray.forEach(elem => {
+    if (elem.id === elemID) elem.hidden = false;
+    else elem.hidden = true;
   })
 }
 
-navElemHome.addEventListener('pointerdown', function() {
-  displayElement('home-content')
+homeTabBtn.addEventListener('pointerdown', function() {
+  displayTab('home-content')
 })
 
-navElemMenu.addEventListener('pointerdown', function() {
-  displayElement('menu-content')
+menuTabBtn.addEventListener('pointerdown', function() {
+  displayTab('menu-content')
 })
 
-navElemContact.addEventListener('pointerdown', function() {
-  displayElement('contact-content')
+ContactTabBtn.addEventListener('pointerdown', function() {
+  displayTab('contact-content')
 })
