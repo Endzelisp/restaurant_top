eb07fbac832787c1a7a55b103186dd444fe976f7
Endzelisp <endzelisp@gmail.com>
2023-11-29 23:19:53 -0400

    Update aria-selected value on pointed tab

--- src/index.js
+++ src/index.js
@@ -9,19 +9,19 @@ const tabs = document.querySelectorAll('#main-nav-bar > button[role="tab"]');
 // main area where the content is displayed
 const divContentArea = document.querySelector('div#content-area');
 
-const tabArray = [
+const tabPanelsArray = [
   homeSection,
   menuSection,
   contactSection,
 ];
 
-divContentArea.append(...tabArray);
+divContentArea.append(...tabPanelsArray);
 
 /**
  *  @param {string} elemID 'Element ID'
  */
-const displayTab = function(elemID) {
-  tabArray.forEach(elem => {
+const displayTabPanel = function(elemID) {
+  tabPanelsArray.forEach(elem => {
     const ariaLabelledby = elem.getAttribute('aria-labelledby');
     if (ariaLabelledby === elemID) {
       elem.hidden = false;
@@ -31,7 +31,16 @@ const displayTab = function(elemID) {
 };
 
 tabs.forEach(function(tab) {
-  tab.addEventListener('pointerdown', function() {
-    displayTab(this.id)
+  // add an event listener to each nav menu tab
+  tab.addEventListener('pointerdown', function(e) {
+    // get the pointed tab
+    const pointedTab = e.currentTarget;
+    // display the corresponding tab panel
+    displayTabPanel(this.id)
+    // loop over each tab to update the aria-selected attr
+    tabs.forEach(function(currentTab) {
+      const isSelected = pointedTab.id === currentTab.id;
+      currentTab.setAttribute('aria-selected', isSelected);
+    })
   })
 })
